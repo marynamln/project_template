5d89bc015e6abdcd04f38d52d8a6f693189435d9
Maryna Melnyk <melnikm535@gmail.com>
2024-03-12 13:01:53 +0200

    input output functions

---
+++ app/io/input.py
@@ -0,0 +1,10 @@
+def input_text():
+    pass
+
+
+def read_file(file_path):
+    pass
+
+
+def read_file_pandas(file_path):
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,6 @@
+def output_text(text):
+    pass
+
+
+def write_to_file(file_path, text):
+    pass
